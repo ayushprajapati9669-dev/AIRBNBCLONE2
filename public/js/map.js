67ed8355ee63dbd4bd0d011797531864c5d009b5
Ayush Prajapati <ayushprajapati9669@gmail.com>
2026-06-24 13:26:49 +0530

    map features half added

---
+++ public/js/map.js
@@ -0,0 +1,9 @@
+﻿const map = L.map('map').setView([28.6139, 77.2090], 11);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
+
+L.marker([28.6139, 77.2090]).addTo(map)
+      .bindPopup("New Delhi")
+      .openPopup();
